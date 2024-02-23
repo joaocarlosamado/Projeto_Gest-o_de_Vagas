9a90857be101b09974d33800747126496c36b298
joao <joaocarlos3362@gmail.com>
2024-02-23 11:09:16 -0300

    add validacion username and email(candidateEntity)

--- src/main/java/br/com/joaocarlos/gestao_de_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/joaocarlos/gestao_de_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.joaocarlos.gestao_de_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data //criar todos os set e get
@@ -9,8 +13,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+    
+    //não permitir usa espaço 
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço") //expressao regular
     private String username;
+
+    @Email(message = "O Campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 6, max = 8)
     private String password;
     private String description;
     private String curriculum;
