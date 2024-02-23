18ad702a4ec35ca4897a1d9bf880736ea7f21a85
joao <joaocarlos3362@gmail.com>
2024-02-23 12:47:30 -0300

    add ErrorMessageDTO.java

---
+++ src/main/java/br/com/joaocarlos/gestao_de_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,13 @@
+package br.com.joaocarlos.gestao_de_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    
+
+    private String message;
+    private String field;
+}
