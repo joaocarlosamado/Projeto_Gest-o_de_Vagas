f6672c71a2866a31d22c30aafcaadd96157581de
joao <joaocarlos3362@gmail.com>
2024-02-23 10:42:59 -0300

    add @RequestBody

--- src/main/java/br/com/joaocarlos/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/joaocarlos/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,18 +1,22 @@
 package br.com.joaocarlos.gestao_de_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaocarlos.gestao_de_vagas.modules.candidate.CandidateEntity;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
 
-    
-    @PostMapping("/")
-    public void create(){
 
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity){
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
     }
     
 }
